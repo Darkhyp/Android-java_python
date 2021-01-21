eea68f220c8e037c94f581a8a8e2461c961e680d
Darkhyp <a_v_kor73@rambler.ru>
2021-01-21 23:51:27 +0100

    empty string correction

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,3 +1,7 @@
-def main(number1, number2):
+def main(number1:str, number2:str):
+    if number1=="":
+        number1 = 0
+    number2 = 0 if number2=="" else number2
+
     sum = int(number1) + int(number2)
     return f"Sum of {number1} and {number2} is {sum}"
